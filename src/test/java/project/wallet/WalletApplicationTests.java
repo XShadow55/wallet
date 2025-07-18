783a85f2ed72ec3c158766ede686c8b0d6ed7204
Свинухов константин <lopata216@yandex.ru>
2025-07-18 16:02:09 +0300

    Пустышка

---
+++ src/test/java/project/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package project.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
